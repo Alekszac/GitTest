ba0b79249e110a11e9238f950742fab0325bdf49
Alekszac <aleks.zac@gmail.com>
2022-07-30 19:56:28 +0300

    Comments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
+// новый комментарий
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
